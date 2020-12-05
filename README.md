70aa0728cfc13909037e95d13be1d81798ec9832
jlopinto <julien.lopinto@gmail.com>
2020-12-06 00:30:09 +0100

    fixing build badge

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-[![Build Status](https://travis-ci.org/jlopinto/dne.png?branch=main)](https://travis-ci.org/jlopinto/dne)
+MAIN: [![Build Status](https://travis-ci.org/jlopinto/nde.svg?branch=main)](https://travis-ci.org/github/jlopinto/nde)
+DEV: [![Build Status](https://travis-ci.org/jlopinto/nde.svg?branch=dev)](https://travis-ci.org/github/jlopinto/nde)
 
 # nde documentation
 Incoming soon !
--- dist/src.9caef6c7.js
+++ dist/src.9caef6c7.js
@@ -232,7 +232,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59249" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65114" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
